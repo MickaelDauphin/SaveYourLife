5e6f04fa6f0f11daa96ea7d09d7892e73e97797c
Louis GRANGE <46597365+Kraknistic@users.noreply.github.com>
2019-10-18 10:13:29 +0200

    ECHELLE ajouter

---
+++ SaveYourLife/Assets/Scripts/Echelle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+public class Echelle : MonoBehaviour
+{
+    public bool Monte = false;
+    public float vitesse = 8f;
+    public GameObject Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Monte)
+        {
+            print("JE MONTE LECHELLE!!");
+            Player.transform.Translate(Vector3.up * vitesse * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Monte = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Monte = false;
+        }
+    }
+}
