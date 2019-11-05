cc76bf825544c95f38d229c8c6d8cdb61a9a87e5
recrespe <remy.crespe@etu.uca.fr>
2019-11-05 11:07:31 +0100

    tangage bateau

--- SaveYourLife/Assets/Scripts/testtangage.cs
+++ SaveYourLife/Assets/Scripts/testtangage.cs
@@ -6,12 +6,7 @@ public class testtangage : MonoBehaviour
     {
 
     public GameObject cube1;
-    public int temps1;
-    public int xAngle = 20;
     public float speed =2.0f;
-    private float coef;
-    private bool IsRotateRight;
-    private float temps;
     private bool b;
 
 
@@ -24,30 +19,23 @@ public class testtangage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Scube1.transform.Rotate(5, 0, 0);
-
-        if (xAngle > 0)
-            coef = -1;
-        else
-            coef = 1;
-
         b = true;
     }
 
 
-  
+
 
     // Update is called once per frame
     void Update()
     {
 
 
- 
+
         if (b)
         {
 
-            transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, timeCount * speed);
-            timeCount = timeCount + Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, timeCount);
+            timeCount = timeCount + Time.deltaTime * speed;
             if (timeCount >= 0.99)
             {
                 b = false;
@@ -58,8 +46,8 @@ public class testtangage : MonoBehaviour
         else
         {
 
-            transform.rotation = Quaternion.Slerp(to.rotation, from.rotation, timeCount * speed);
-            timeCount = timeCount + Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(to.rotation, from.rotation, timeCount);
+            timeCount = timeCount + Time.deltaTime * speed;
             if (timeCount >= 0.99)
             {
                 b = true;
@@ -67,84 +55,6 @@ public class testtangage : MonoBehaviour
             }
 
         }
-
-
-
-
-
-        /*
-        print(ConvertAngle(transform.localEulerAngles.x));
-        if (ConvertAngle(transform.localEulerAngles.x) <= xAngle)
-        {
-            //transform.Rotate(new Vector3(100 * coef, 0, 0) * Time.deltaTime);
-            transform.Rotate(new Vector3(speed * coef, 0, 0) * Time.deltaTime);
-        }
-        else if (xAngle > 0)
-        {
-            xAngle = -xAngle;
-
-        }
-
-
-        if (ConvertAngle(transform.localEulerAngles.x) > xAngle)
-        {
-            //transform.Rotate(new Vector3(100 * coef, 0, 0) * Time.deltaTime);
-            transform.Rotate(new Vector3(speed * -coef, 0, 0) * Time.deltaTime);
-        }
-        else if (xAngle < 0)
-        {
-            xAngle = -xAngle;
-        }
-
-
-
-
-
-        print("Tourne vers " + coef);
-            print("rapproche de l'angle " + xAngle); 
-        */
-
-        //    temps += Time.deltaTime;
-        //    if(temps>=temps1 && IsRotateRight)
-        //    {
-        //        //for (int i = 0; i <= xAngle; i++)
-        //        //{
-        //        //   cube1.transform.Rotate(i*Time.deltaTime, 0, 0);
-        //        //}
-
-
-
-        //        //transform.Rotate(new Vector3(xAngle, 0, 0) * Time.deltaTime);
-        //        //transform.Rotate(new Vector3(0, 100 * 45, 0) * Time.deltaTime);
-
-
-
-
-        //        temps = 0;
-        //        IsRotateRight = !IsRotateRight;
-        //    }
-        //    else if(temps>=temps1 && !IsRotateRight)
-        //    {
-
-
-        //        //for (int i = 0; i <= xAngle; i++)
-        //        //{
-        //        //    cube1.transform.Rotate(-i, 0, 0);
-        //        //}
-
-
-
-        //        cube1.transform.Rotate(-xAngle, 0, 0);
-        //        //cube1.transform.Rotate(-xAngle*Time.deltaTime, 0, 0);
-        //        temps = 0;
-        //        IsRotateRight = !IsRotateRight;
-        //    }
-        //}
     }
-    float ConvertAngle(float angle)
-    {
-        if (angle > 180)
-            angle = angle - 360;
-        return angle;
-    }
+       
 }
