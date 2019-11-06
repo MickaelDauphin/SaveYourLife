ec0f0e01f1fda882fe8359f72248f7ccccca1159
Louis GRANGE <46597365+Kraknistic@users.noreply.github.com>
2019-11-06 19:28:25 +0100

    Porte + levier

---
+++ SaveYourLife/Assets/Scripts/Levier.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levier : MonoBehaviour
+{
+    private float posBas = -40;
+    private float posMid = 0;
+    private float posHaut = 40;
+    private float posActu;
+    public int NumeroDuLevier = 0;
+    public int ValeurL1, ValeurL2, ValeurL3, ValeurL4;
+    private int Total;
+   
+    private void OnMouseDown()
+    {
+        posActu = this.GetComponent<Transform>().localEulerAngles.x; //this.GetComponent<Transform>().rotation.x;
+        print(posActu);
+        if(posActu == posMid)
+        {
+            this.GetComponent<Transform>().Rotate(posBas,0,0);
+            if (NumeroDuLevier == 1) { ValeurL1 = 1; }
+            if (NumeroDuLevier == 2) { ValeurL2 = 10; }
+            if (NumeroDuLevier == 3) { ValeurL3 = 100; }
+            if (NumeroDuLevier == 4) { ValeurL4 = 1000; }
+        }
+        else if (posActu == posBas + 360)
+        {
+            this.GetComponent<Transform>().Rotate(80, 0, 0);
+            if (NumeroDuLevier == 1) { ValeurL1 = 2; }
+            if (NumeroDuLevier == 2) { ValeurL2 = 20; }
+            if (NumeroDuLevier == 3) { ValeurL3 = 200; }
+            if (NumeroDuLevier == 4) { ValeurL4 = 2000; }
+        }
+        else if (posActu == posHaut)
+        {
+            this.GetComponent<Transform>().Rotate(-40, 0, 0);
+            if (NumeroDuLevier == 1) { ValeurL1 = 3; }
+            if (NumeroDuLevier == 2) { ValeurL2 = 30; }
+            if (NumeroDuLevier == 3) { ValeurL3 = 300; }
+            if (NumeroDuLevier == 4) { ValeurL4 = 3000; }
+        }
+    }
+}
---
+++ SaveYourLife/Assets/Scripts/WoodDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool CanOpen;
+    float Rotation;
+    int y = 0;
+    public int maxOpen;
+    private bool isDo = false;
+    public GameObject L1, L2, L3, L4;
+    void Start()
+    {
+        CanOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (L1.GetComponent<Levier>().ValeurL1 + L2.GetComponent<Levier>().ValeurL2 + L3.GetComponent<Levier>().ValeurL3 + L4.GetComponent<Levier>().ValeurL4 == 2321)
+            CanOpen = true;
+        if (CanOpen && transform.localEulerAngles.y < maxOpen && maxOpen > 0 && maxOpen < 180)
+        {
+            transform.Rotate(new Vector3(0, 100, 0) * Time.deltaTime);
+        }
+        if (CanOpen && maxOpen > 180)
+        {
+            if (transform.localEulerAngles.y <= 45 && transform.localEulerAngles.y > 0 || transform.localEulerAngles.y > maxOpen)
+            {
+                transform.Rotate(new Vector3(0, -100, 0) * Time.deltaTime);
+            }
+        }
+    }
+}
