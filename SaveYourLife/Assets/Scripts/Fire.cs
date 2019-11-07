d7279356be0431878fdc063cb67d37c23e49b86d
DAUPHIN Mickael <mickael.dauphin@etu.uca.fr>
2019-11-07 22:30:15 +0100

    Ajout flames

---
+++ SaveYourLife/Assets/Scripts/Fire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : INTERACTION_CLICK_AND_PICK
+{
+    public GameObject player;
+    public ParticleSystem particle;
+    private void Start()
+    {
+        particle.Stop();
+    }
+    private void OnMouseDown()
+    {
+        if (player.GetComponent<Inventory>().HaveTorch() && player.GetComponent<Inventory>().StrawTop)
+        {
+            particle.Play();
+            player.GetComponent<Inventory>().Torch.SetActive(false);
+            base.Declencher_Etape_Suivante_Du_Scenario();
+        }
+    }
+}
